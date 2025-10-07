9dd60de91f18f73c2f8d28be7c5185838667f875
kingsolomon-s <kingsolomon.sobande1@gmail.com>
2025-10-07 23:56:14 +0100

    Feat: Add objective to inform users the rules of the game

--- src/components/pages/home-page.jsx
+++ src/components/pages/home-page.jsx
@@ -3,6 +3,11 @@ import "../../styles/home-page.css";
 function Home({ setPage }) {
   return (
     <div className="home-container">
+      <div className="objective-container">
+        <h3>OBJECTIVE</h3>
+        <p>Get points by clicking on an image card.</p>
+        <p>Click each card only once — no repeats!</p>
+      </div>
       <button className="play-button" onClick={() => setPage("level")}>
         Play Now
       </button>
